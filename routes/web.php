7e1c69516f298cd1c5d5041ea520d0bb1759feb5
ZanThi268 <030239230282@st.buh.edu.vn>
2026-04-14 09:20:23 +0700

    Cau 2 + 5

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,8 @@ use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', [HomeController::class, 'index']);
+Route::get('/laptop/theloai/{id}', [HomeController::class, 'theoDanhMuc']);
+Route::post('/timkiem', [HomeController::class, 'timKiem']);
 
 Route::get('/dashboard', function () {
     return view('dashboard');
